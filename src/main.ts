dc5f7afc9dc4ff3ff3f9d1627b4b67bce88e208f
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2024-02-04 15:48:58 +0900

    receive message, create page

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,94 @@
 import { App } from "@slack/bolt";
+import { Client } from "@notionhq/client";
 
-const app = new App({
+const ParagraphBlock = {
+  crateParagraph(text: string) {
+    return {
+      paragraph: {
+        rich_text: [
+          {
+            text: {
+              content: text,
+            },
+          },
+        ],
+      },
+    };
+  },
+  crateParagraphWithLink(text: string, url: string) {
+    return {
+      paragraph: {
+        rich_text: [
+          {
+            text: {
+              content: text,
+              link: {
+                url,
+              },
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const slack = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.event("reaction_added", async ({ event, logger }) => {
-  logger.info(event);
-  if (event.reaction === "heart") {
-  }
+slack.event("reaction_added", async ({ event, client, logger }) => {
+  const { item, reaction } = event;
+  if (reaction !== "+1") return;
+
+  const resReplies = await client.conversations.replies({
+    channel: `${item.channel}`,
+    ts: `${item.ts}`,
+  });
+  if (resReplies.messages === undefined) return;
+  // TODO: rich text対応(messages[x].block)、
+  // https://api.slack.com/reference/block-kit/blocks#rich_text
+  // TODO: 念の為時間でソート
+  const slackTextList = resReplies.messages.map((msg) => msg.text ?? "");
+
+  // TODO: rich text 対応
+  // https://developers.notion.com/reference/rich-text
+  const notionTextList = slackTextList
+    .slice(1)
+    .map((msg) => ParagraphBlock.crateParagraph(msg));
+
+  const pageId = process.env.NOTION_PAGE_ID;
+  const newPage = await notion.pages.create({
+    parent: {
+      type: "page_id",
+      page_id: pageId ?? "",
+    },
+    properties: {
+      title: {
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: slackTextList[0] ?? "",
+              link: null,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  await notion.blocks.children.append({
+    block_id: newPage.id,
+    children: notionTextList,
+  });
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  await slack.start(process.env.PORT || 3000);
 
-  console.log("app running");
+  console.log("slack running");
 })();
